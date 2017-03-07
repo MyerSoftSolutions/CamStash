faddd8c292535ab894fa799f7b193d63fdc76d6b
Joel Myers <myersoftsolutions@gmail.com>
2017-03-07 18:07:07 -0500

    Initial Commit

---
+++ CamStash/CamStash/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CamStash
+//
+//  Created by Joel Myers on 3/7/17.
+//  Copyright © 2017 Joel Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
